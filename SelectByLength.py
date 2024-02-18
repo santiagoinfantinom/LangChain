dc1359b8e2c43b131ab71a45f26274aa29b10f81
Santiago Infantino <infantinomoreno@campus.tu-berlin.de>
2024-02-18 13:33:47 +0100

    Use LengthBaseSelector to pick with a max length

---
+++ SelectByLength.py
@@ -0,0 +1,40 @@
+from langchain.prompts import FewShotPromptTemplate, PromptTemplate
+from langchain.prompts.example_selector import LengthBasedExampleSelector
+
+# Examples of a pretend task of creating antonyms.
+examples = [
+    {"input": "happy", "output": "sad"},
+    {"input": "tall", "output": "short"},
+    {"input": "energetic", "output": "lethargic"},
+    {"input": "sunny", "output": "gloomy"},
+    {"input": "windy", "output": "calm"},
+]
+
+example_prompt = PromptTemplate(
+    input_variables=["input", "output"],
+    template="Input: {input}\nOutput: {output}",
+)
+example_selector = LengthBasedExampleSelector(
+    # The examples it has available to choose from.
+    examples=examples,
+    # The PromptTemplate being used to format the examples.
+    example_prompt=example_prompt,
+    # The maximum length that the formatted examples should be.
+    # Length is measured by the get_text_length function below.
+    max_length=25,
+    # The function used to get the length of a string, which is used
+    # to determine which examples to include. It is commented out because
+    # it is provided as a default value if none is specified.
+    # get_text_length: Callable[[str], int] = lambda x: len(re.split("\n| ", x))
+)
+dynamic_prompt = FewShotPromptTemplate(
+    # We provide an ExampleSelector instead of examples.
+    example_selector=example_selector,
+    example_prompt=example_prompt,
+    prefix="Give the antonym of every input",
+    suffix="Input: {adjective}\nOutput:",
+    input_variables=["adjective"],
+)
+
+# An example with small input, so it selects all examples.
+print(dynamic_prompt.format(adjective="big"))
